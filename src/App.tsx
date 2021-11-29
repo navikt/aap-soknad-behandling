76f60605b51cacc62907c9106a9037eeb8ee5167
Tor Idland <tor.idland@nav.no>
2021-11-29 16:01:02 +0100

    client

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Heading } from "@navikt/ds-react";
+import "@navikt/ds-css";
+
+export function App() {
+    return (<Heading size={"2xlarge"} level={"1"} >Hello world!</Heading>);
+}
