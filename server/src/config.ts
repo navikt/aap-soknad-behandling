30d968a8c1397fc105644e26e44cd447a58eaa48
Tor Idland <tor.idland@nav.no>
2021-11-29 16:21:11 +0100

    server

---
+++ server/src/config.ts
@@ -0,0 +1,6 @@
+const APP_URL = process.env.APP_URL || 'localhost:3000'
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
+export default {
+  BASE_PATH: '/aap-behandling',
+  APP_URL,
+};
---
+++ server/src/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import client from 'prom-client';
+import config from './config';
+import {LogInfo} from "./logger";
+
+
+const BUILD_PATH = path.join(__dirname, "../build");
+const PORT = process.env.PORT || 3000;
+const server = express();
+
+const startServer = () => {
+  // Create a Registry which registers the metrics
+  const register = new client.Registry();
+  // Add a default label which is added to all metrics
+  register.setDefaultLabels({
+    app: 'aap-soknad-behandling'
+  });// Enable the collection of default metrics
+  client.collectDefaultMetrics({ register });
+
+  server.use(cookieParser());
+  server.use(express.static(BUILD_PATH));
+
+  // metrics
+  server.get(`${config.BASE_PATH}/internal/metrics`, async (req: any, res: any) => {
+    res.setHeader('Content-Type', register.contentType);
+    res.end(await register.metrics());
+  });
+  // health checks
+  server.get([`${config.BASE_PATH}/internal/isAlive`, `${config.BASE_PATH}/internal/isReady`], (req: any, res: any) =>
+    res.sendStatus(200)
+  );
+
+  // Render app
+  server.get(`${config.BASE_PATH}/*`, (req: any, res: any) =>
+      res.sendFile(path.join(BUILD_PATH, 'index.html'))
+  );
+
+  server.listen(PORT, () => {
+    LogInfo(`Server started: listening on port ${PORT}`);
+  });
+};
+
+startServer();
