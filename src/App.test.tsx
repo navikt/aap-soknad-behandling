b15ae2a70110d434670b770690fb9218385534d3
Tor Idland <tor.idland@nav.no>
2022-01-17 13:54:07 +0100

    test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,7 @@ import { App } from "./App";
 describe('my tests', () => {
     test("helloworld", () => {
         render(<App />);
-        const linkElement = screen.getByText(/Hello world!/i);
+        const linkElement = screen.getByText(/Oppgaver/i);
         expect(true).toBe(true);
     });
 });
