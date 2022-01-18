192f6f2959db149e940d6c451c8659e97fa7c9e5
Tor Idland <tor.idland@nav.no>
2022-01-18 15:33:36 +0100

    oppgaver med vurderAlder
    Co-authored-by: Anders Østby <anders.ostby@nav.no>
    Co-authored-by: Robin Tordly <robin.tordly@nav.no>

--- src/Oppgave.tsx
+++ src/Oppgave.tsx
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from "react";
-import {BodyShort, Heading} from "@navikt/ds-react";
+import {BodyShort, Heading, Button} from "@navikt/ds-react";
 type Oppgave = {
-    personIdent: string;
+    oppgaveId: number;
+    personident: string;
     alder: number;
 }
 const Oppgave = () => {
@@ -10,16 +11,30 @@ const Oppgave = () => {
         const getOppgaver = async () => {
             try {
                 const oppgaveResponse = await fetch('/aap-behandling/api/oppgaver').then(res => res.json());
-                setOppgaver(oppgaveResponse);
+                oppgaveResponse.oppgaver && setOppgaver(oppgaveResponse.oppgaver);
             } catch (err) {
                 console.error(err);
             }
         }
         getOppgaver();
     }, []);
+    const onAgeConfirm = async (erMellom18og67: boolean, oppgaveId: number) => {
+        const payload ={
+            oppgaveId,
+            erMellom18og67
+        }
+        await fetch('/aap-behandling/api/vurderAlder', {
+            method: 'POST',
+            body: JSON.stringify(payload),
+        }).then(res => console.log(res.status));
+    }
     return (<>
         <Heading size={"2xlarge"} level={"1"} >Oppgaver</Heading>
-        {oppgaver.map( oppgave => (<BodyShort>{`PersonIdent: ${oppgave.personIdent}, Alder: ${oppgave.alder}`}</BodyShort>))}
+        {oppgaver.map( oppgave => <>
+            <BodyShort>{`Personident: ${oppgave.personident}, Alder: ${oppgave.alder}`}</BodyShort>
+            <Button variant="primary" type="button" onClick={() => onAgeConfirm(true, oppgave.oppgaveId)}>Ja</Button>
+            <Button variant="tertiary" onClick={() => onAgeConfirm(false, oppgave.oppgaveId)}>Nei</Button>
+            </>)}
         </>);
 }
 
