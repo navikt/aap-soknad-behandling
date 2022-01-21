4ac6c7e0af968d2fe2221f478eca08a48a3e604a
Tor Idland <tor.idland@nav.no>
2022-01-21 10:35:20 +0100

    react-router-dom
    Co-authored-by: Øivind Stensrud <oivind.stensrud@nav.no>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { Heading } from "@navikt/ds-react";
 import Oppgave from "./Oppgave";
 
 export function App() {
-    return (<Oppgave />);
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/soknad-behandling/" element={<Oppgave />} />
+                <Route
+                    path="*"
+                    element={
+                        <>
+                            <Heading size={"2xlarge"} level={"1"} spacing={true}>
+                                AAP App
+                            </Heading>
+                            <span>Not Found</span>
+                        </>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
+        );
 }
