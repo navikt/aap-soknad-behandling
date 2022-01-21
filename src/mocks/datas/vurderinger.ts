0c2434df1328504abaf22ae505514a149c176fda
Øivind Stensrud <oivind.stensrud@nav.no>
2022-01-21 09:22:42 +0100

    Oppsett for msw

---
+++ src/mocks/datas/vurderinger.ts
@@ -0,0 +1,43 @@
+const testdataliste = [
+  {
+    personident: "11068812345",
+    fødselsdato: "1988-06-11",
+    vilkårsvurdering: [
+      {
+        vilkår: {
+          paragraf: "11-4",
+          ledd: "1",
+        },
+        tilstand: "OPPFYLT",
+      },
+    ],
+  },
+  {
+    personident: "27109500123",
+    fødselsdato: "1995-10-27",
+    vilkårsvurdering: [
+      {
+        vilkår: {
+          paragraf: "11-4",
+          ledd: "1",
+        },
+        tilstand: "IKKE_OPPFYLT",
+      },
+    ],
+  },
+  {
+    personident: "01090200432",
+    fødselsdato: "2002-09-01",
+    vilkårsvurdering: [
+      {
+        vilkår: {
+          paragraf: "11-4",
+          ledd: "1",
+        },
+        tilstand: "OPPFYLT",
+      },
+    ],
+  },
+];
+
+export { testdataliste };
