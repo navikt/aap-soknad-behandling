fac97753abefd9dc8fc72154c5376dc97f5f681c
Øivind Stensrud <oivind.stensrud@nav.no>
2022-03-29 13:44:17 +0200

    Layout-justeringer for vedtak

--- src/components/Sak/vedtak/Vedtak.tsx
+++ src/components/Sak/vedtak/Vedtak.tsx
@@ -1,40 +1,57 @@
 import { SøkerType } from "../../../types/SakType";
-import { Heading } from "@navikt/ds-react";
+import { BodyShort, Heading, Table } from "@navikt/ds-react";
 import { formaterDato } from "../../../lib/dato";
 import * as styles from "./vedtak.module.css";
-import {InntektsgrunnlagType} from "../../../types/Vedtak";
+import { InntektsgrunnlagType } from "../../../types/Vedtak";
 
 const Inntektsgrunnlag = ({ inntekt }: { inntekt: InntektsgrunnlagType }): JSX.Element => {
   return (
     <>
-      <Heading size={"medium"}>Beregning av utbetaling</Heading>
-      <div>Grunnlaget for beregning er beregningsdato {formaterDato(inntekt.beregningsdato)}</div>
-      <Heading size={"small"}>Inntektsgrunnlag</Heading>
-      <ul>
-        {
-          inntekt.inntekterSiste3Kalenderår.map(inntekt => <li key={"1"}>{inntekt.år} {inntekt.beløpFørJustering}kr</li>)
-        }
-      </ul>
+      <Heading level={"2"} size={"medium"}>
+        Beregning av utbetaling
+      </Heading>
+      <BodyShort>Beregningsdato er {formaterDato(inntekt.beregningsdato)}</BodyShort>
+      <Heading level={"3"} size={"small"}>
+        Inntektsgrunnlag
+      </Heading>
+      <Table className={styles.inntektstabell} zebraStripes>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>År</Table.HeaderCell>
+            <Table.HeaderCell>Inntekt</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {inntekt.inntekterSiste3Kalenderår.map((inntekt) => (
+            <Table.Row key={inntekt.år}>
+              <Table.DataCell>{inntekt.år}</Table.DataCell>
+              <Table.DataCell>{inntekt.beløpFørJustering}</Table.DataCell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
     </>
   );
-}
+};
 
 const Vedtak = ({ søker }: { søker: SøkerType }): JSX.Element => {
   const vedtak = søker.sak.vedtak;
-  if(!vedtak) {
-    return (<div>Ingen vedtak fattet enda</div>);
+  if (!vedtak) {
+    return <div>Ingen vedtak fattet enda</div>;
   }
 
   return (
-    <div>
-      <Heading size={"large"}>Vedtak er {vedtak.innvilget ? 'innvilget' : 'ikke innvilget'}</Heading>
-      <div className={styles.kolonne}>
-        <span>Vedtak ble innvilget {formaterDato(vedtak.vedtaksdato)}</span>
-        <span>Virkningstidspunkt er {formaterDato(vedtak.virkningsdato)}</span>
-      </div>
+    <section className={styles.vedtak__blokk}>
+      <Heading level={"2"} size={"medium"}>
+        Vedtak er {vedtak.innvilget ? "innvilget" : "ikke innvilget"}
+      </Heading>
+      <section>
+        <BodyShort>Vedtak ble innvilget {formaterDato(vedtak.vedtaksdato)}</BodyShort>
+        <BodyShort>Virkningstidspunkt er {formaterDato(vedtak.virkningsdato)}</BodyShort>
+      </section>
       <Inntektsgrunnlag inntekt={vedtak.inntektsgrunnlag} />
-    </div>
+    </section>
   );
-}
+};
 
 export default Vedtak;
