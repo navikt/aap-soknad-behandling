5f21795bb47fd737e335c1d8332343756ffb0759
Øivind Stensrud <oivind.stensrud@nav.no>
2022-04-05 11:08:10 +0200

    React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import {render } from "react-dom";
 import { App } from "./App";
+import { createRoot } from "react-dom/client";
 import "@navikt/ds-css";
 import "@navikt/ds-css-internal";
 import "./index.css";
 import "./globals.css";
 
 if (process.env.USE_MOCK) {
-  const { worker } = require('./mocks/browser')
-  worker.start()
+  const { worker } = require("./mocks/browser");
+  worker.start();
 }
 
-const app = document.getElementById("app");
-render(<div className="app-container">
-  <App />
-</div>, app);
+const container = document.getElementById("app");
+const root = createRoot(container as NonNullable<HTMLElement>);
+root.render(
+  <div className={"app-container"}>
+    <App />
+  </div>
+);
