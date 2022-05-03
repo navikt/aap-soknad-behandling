100219a7e2153b7b0af87e300edf4d29e458f44d
Øivind Stensrud <oivind.stensrud@nav.no>
2022-05-03 16:28:16 +0200

    Tegn sakslenker som router-lenker for å hindre komplett redraw

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,45 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import { App } from "./App";
-describe('APP', () => {
-    test("Tegner app-en", () => {
-        render(<App />);
-        expect(screen.getByText("AAP App")).toBeVisible();
-    });
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
+import { App, Ruter } from "./App";
+import { MemoryRouter } from "react-router-dom";
+
+describe("APP", () => {
+  test("Tegner app-en", () => {
+    render(<App />);
+    expect(screen.getByText("AAP Vedtaksløsning")).toBeVisible();
+  });
+
+  test("gir 404 når stien ikke finnes", () => {
+    render(
+      <MemoryRouter initialEntries={["/finnes-ikke"]}>
+        <Ruter />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText("AAP App")).toBeVisible();
+  });
+
+  test("Sender deg til saksoversikt når sti ikke er angitt", async () => {
+    render(
+      <MemoryRouter initialEntries={[""]}>
+        <Ruter />
+      </MemoryRouter>
+    );
+    const venterElement = screen.getByText("venter...");
+    expect(venterElement).toBeInTheDocument();
+    await waitForElementToBeRemoved(venterElement);
+    expect(screen.getByText("Oppgaver AAP")).toBeVisible();
+  });
+
+  test("Sender deg til saksoversikt når bare deler av sti er angitt", async () => {
+    render(
+      <MemoryRouter initialEntries={["/aap-behandling"]}>
+        <Ruter />
+      </MemoryRouter>
+    );
+    const venterElement = screen.getByText("venter...");
+    expect(venterElement).toBeInTheDocument();
+    await waitForElementToBeRemoved(venterElement);
+    expect(screen.getByText("Oppgaver AAP")).toBeVisible();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ErrorBoundary } from "react-error-boundary";
 
 import { Heading } from "@navikt/ds-react";
@@ -9,39 +9,48 @@ import { Feilviser } from "./components/Feilviser/Feilviser";
 import { AppHeader } from "./components/Header/AppHeader";
 import { SakHenter } from "./components/SakHenter/SakHenter";
 
-export function App() {
+const Ruter = (): JSX.Element => (
+  <Routes>
+    <Route path={"/aap-behandling"}>
+      <Route
+        path="saksoversikt"
+        element={
+          <ErrorBoundary FallbackComponent={Feilviser}>
+            <Saksoversikt />
+          </ErrorBoundary>
+        }
+      />
+      <Route path="sak/:personid" element={<SakHenter />} />
+      <Route path={""} element={<Navigate to="saksoversikt" />} />
+    </Route>
+    <Route path={""} element={<Navigate to="/aap-behandling/saksoversikt" />} />
+    <Route
+      path="*"
+      element={
+        <>
+          <Heading size={"xlarge"} level={"1"} spacing={true}>
+            AAP App
+          </Heading>
+          <span>Not Found</span>
+        </>
+      }
+    />
+  </Routes>
+);
+
+const App = () => {
   return (
     <StrictMode>
       <ErrorBoundary FallbackComponent={Feilviser}>
         <AppHeader />
         <main id="main-content" className="main__content">
           <BrowserRouter>
-            <Routes>
-              <Route
-                path="/aap-behandling/saksoversikt"
-                element={
-                  <ErrorBoundary FallbackComponent={Feilviser}>
-                    <Saksoversikt />
-                  </ErrorBoundary>
-                }
-              />
-              <Route path="/aap-behandling/sak/neste" element={<SakHenter />} />
-              <Route path="/aap-behandling/sak/:personid" element={<SakHenter />} />
-              <Route
-                path="*"
-                element={
-                  <>
-                    <Heading size={"xlarge"} level={"1"} spacing={true}>
-                      AAP App
-                    </Heading>
-                    <span>Not Found</span>
-                  </>
-                }
-              />
-            </Routes>
+            <Ruter />
           </BrowserRouter>
         </main>
       </ErrorBoundary>
     </StrictMode>
   );
-}
+};
+
+export { App, Ruter };
