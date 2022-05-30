439c243db31cde0c0a7ae67f679ed17bcdd0cdc2
Øivind Stensrud <oivind.stensrud@nav.no>
2022-05-30 13:30:15 +0200

    Støtte for å skjule deler av header

--- src/components/Sak/Sammendrag/Sammendrag.tsx
+++ src/components/Sak/Sammendrag/Sammendrag.tsx
@@ -1,11 +1,10 @@
-import { Button, Heading, Link, Tag } from "@navikt/ds-react";
-import { Copy } from "@navikt/ds-icons";
-import { finnAlder, formaterPid, formaterTidspunkt } from "../../../lib/dato";
-import { Personopplysninger } from "../Personopplysninger";
-import { SøkerType } from "../../../types/SakType";
-
-import * as styles from "./sammendrag.module.css";
 import { useState } from "react";
+import { Button, Heading, Link, Tag } from "@navikt/ds-react";
+import { Back, Copy, Next } from "@navikt/ds-icons";
+import { finnAlder, formaterPid, formaterTidspunkt } from "../../../lib/dato";
+import { SøkerType } from "../../../types/SakType";
+import * as styles from "./sammendrag.module.css";
+import { Personopplysninger } from "../Personopplysninger";
 
 const Sammendrag = ({ søker, layoutToggle }: { søker: SøkerType; layoutToggle?: Function }): JSX.Element => {
   const [isCompact, toggleCompact] = useState<boolean>(false);
@@ -13,44 +12,53 @@ const Sammendrag = ({ søker, layoutToggle }: { søker: SøkerType; layoutToggle
     toggleCompact(!isCompact);
     layoutToggle && layoutToggle();
   };
-  const cl = isCompact ? `${styles.oppsummering} ${styles.kompakt}` : `${styles.oppsummering}`;
+  const cl = isCompact ? `${styles.sammendrag} ${styles.kompakt}` : `${styles.sammendrag}`;
+  const wrapperClasses = isCompact
+    ? `${styles.sammendrag__wrapper} ${styles.kompakt}`
+    : `${styles.sammendrag__wrapper}`;
   return (
-    <section className={`${cl} box`}>
-      <div>
-        {søker.sak.vedtak?.innvilget && <Tag variant={"success"}>Vedtak fattet</Tag>}
-        {søker.sak.vedtak?.innvilget === undefined && <Tag variant={"warning"}>Ikke påbegynt</Tag>}
-      </div>
-      <Heading size={"large"} level={"2"}>
-        {søker?.navn}
-        <Button
-          variant={"tertiary"}
-          onClick={() => navigator.clipboard.writeText(søker.navn || "Ukjent navn")}
-          size={"small"}
-        >
-          <Copy title={"Kopier navn til utklippstavlen"} />
-        </Button>
-        <Personopplysninger />
-      </Heading>
-      <div className={styles.oppsummering__personlinje}>
-        <div className={styles.kv__pair}>
-          <span className={styles.key}>Fødselsnummer</span>
-          <span className={styles.value}>{formaterPid(søker.personident)}</span>
-          <Button variant={"tertiary"} onClick={() => navigator.clipboard.writeText(søker.personident)} size={"small"}>
-            <Copy title={"Kopier personid til utklippstavlen"} />
+    <div className={`${wrapperClasses} box`} id={"sammendrag"}>
+      <section className={`${cl}`}>
+        <div>
+          {søker.sak.vedtak?.innvilget && <Tag variant={"success"}>Vedtak fattet</Tag>}
+          {søker.sak.vedtak?.innvilget === undefined && <Tag variant={"warning"}>Ikke påbegynt</Tag>}
+        </div>
+        <Heading size={"large"} level={"2"}>
+          {søker?.navn}
+          <Button
+            variant={"tertiary"}
+            onClick={() => navigator.clipboard.writeText(søker.navn || "Ukjent navn")}
+            size={"small"}
+          >
+            <Copy title={"Kopier navn til utklippstavlen"} />
           </Button>
-          <span>({finnAlder(søker.fødselsdato)} år)</span>
+          <Personopplysninger />
+        </Heading>
+        <div className={styles.sammendrag__personlinje}>
+          <div className={styles.kv__pair}>
+            <span className={styles.key}>Fødselsnummer</span>
+            <span className={styles.value}>{formaterPid(søker.personident)}</span>
+            <Button
+              variant={"tertiary"}
+              onClick={() => navigator.clipboard.writeText(søker.personident)}
+              size={"small"}
+            >
+              <Copy title={"Kopier personid til utklippstavlen"} />
+            </Button>
+            <span>({finnAlder(søker.fødselsdato)} år)</span>
+          </div>
+          <div className={styles.kv__pair}>
+            <span className={styles.key}>Søkte</span>
+            <span className={styles.value}>{formaterTidspunkt(søker.sak.søknadstidspunkt)}</span>
+          </div>
+          <Link href={"#"}>Brukerhistorikk</Link>
         </div>
-        <div className={styles.kv__pair}>
-          <span className={styles.key}>Søkte</span>
-          <span className={styles.value}>{formaterTidspunkt(søker.sak.søknadstidspunkt)}</span>
-        </div>
-        <Link href={"#"}>Brukerhistorikk</Link>
-      </div>
-      <Button onClick={() => changeLayout()} variant={"tertiary"} size={"small"}>
-        {isCompact && ">>"}
-        {!isCompact && "<<"}
+      </section>
+      <Button onClick={() => changeLayout()} variant={"tertiary"}>
+        {isCompact && <Next />}
+        {!isCompact && <Back />}
       </Button>
-    </section>
+    </div>
   );
 };
 
