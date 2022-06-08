4c4eb6fd986eab3dc8421118fd909a89d9313fca
Øivind Stensrud <oivind.stensrud@nav.no>
2022-06-08 14:12:20 +0200

    Oppgraderer pakker

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,14 @@ module.exports = {
   moduleNameMapper: {
     "react-day-picker/dist/style.css": "<rootDir>/src/mocks/styleMock.ts",
   },
+  resolver: `${__dirname}/src/test/resolver.js`,
   transform: {
     ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform",
+    "node_modules/.+\\.(js|jsx)$": "babel-jest",
   },
   setupFilesAfterEnv: ["./setupTests.ts"],
-  testURL: "http://localhost:3000",
+  // testURL: "http://localhost:3000",
+  testEnvironmentOptions: {
+    url: "http://localhost:3000",
+  },
 };
