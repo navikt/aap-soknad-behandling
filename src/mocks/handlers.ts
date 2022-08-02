b5355bab6a49505ceaf90d7132269523a3336fc1
Thomas Rognes <thomas.sebastian.rognes@nav.no>
2022-08-02 12:27:46 +0200

    Rydder opp endepunktene i handlers.ts for å samsvare med backend

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -51,12 +51,16 @@ const settNayAutorisasjon = (person: SøkerType[], erGodkjenner: boolean) =>
 
 export const handlers = (brukertype: Brukertype | undefined, erGodkjenner: boolean | undefined = false) => {
   return [
-    rest.get("/aap-behandling/api/sak", (req, res, ctx) => {
+    rest.get("/api/personopplysninger/:personid", (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json(personopplysning), ctx.delay(523));
+    }),
+
+    rest.get("/api/sak", (req, res, ctx) => {
       return res(ctx.status(200), ctx.json(listeMedSøkereOgSaker), ctx.delay(400));
     }),
-    rest.get("/aap-behandling/api/sak/:personid", (req, res, ctx) => {
-      const { personid } = req.params;
-      let person = listeMedSøkereOgSaker.filter((p) => p.personident === personid);
+    rest.get("/api/sak/:personident", (req, res, ctx) => {
+      const { personident } = req.params;
+      let person = listeMedSøkereOgSaker.filter((p) => p.personident === personident);
       if (brukertype === Brukertype.NAV) {
         person = settNavAutorisasjon(person, erGodkjenner);
       }
@@ -65,29 +69,28 @@ export const handlers = (brukertype: Brukertype | undefined, erGodkjenner: boole
       }
       return res(ctx.status(200), ctx.json(person), ctx.delay(400));
     }),
-    rest.get("/aap-behandling/internal/userinfo", (req, res, ctx) => {
+
+    rest.post("/api/sak/:personident/losning/inngangsvilkar", (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json({ message: "OK" }), ctx.delay(500));
+    }),
+    rest.post("/api/sak/:personident/losning/paragraf_11_5", (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json({ message: "OK" }), ctx.delay(500));
+    }),
+    rest.post("/api/sak/:personident/losning/paragraf_11_6", (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json({ message: "OK" }), ctx.delay(500));
+    }),
+    rest.post("/api/sak/:personident/losning/paragraf_11_29", (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json({ message: "OK" }), ctx.delay(500));
+    }),
+
+    //Denne brukes kun til internt bruk.
+    rest.get("/internal/userinfo", (req, res, ctx) => {
       return res(
         ctx.status(200),
         ctx.json({
           name: "Saks Behandler",
         })
       );
-    }),
-
-    rest.post("/aap-behandling/api/sak/:personident/losning/inngangsvilkar", (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json({ message: "OK" }), ctx.delay(500));
-    }),
-    rest.post("/aap-behandling/api/sak/:personident/losning/paragraf_11_29", (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json({ message: "OK" }), ctx.delay(500));
-    }),
-    rest.post("/aap-behandling/api/sak/:personident/losning/paragraf_11_5", (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json({ message: "OK" }), ctx.delay(500));
-    }),
-    rest.post("/aap-behandling/api/sak/:personident/losning/paragraf_11_6", (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json({ message: "OK" }), ctx.delay(500));
-    }),
-    rest.get("/aap-behandling/api/personopplysninger/:personid", (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json(personopplysning), ctx.delay(523));
     }),
   ];
 };
